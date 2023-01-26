f9b9efabf07a3d496eab2dc21344aa8976b65500
DASaBeR <m.saberi1999@gmail.com>
2023-01-26 22:39:47 +0100

    Add and Implements home.component and fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProjectsComponent
   ],
   imports: [
     BrowserModule,
